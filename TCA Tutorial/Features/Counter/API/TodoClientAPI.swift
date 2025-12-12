591971838f1b90ee09255e78aff4b232e828de0d
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2025-12-12 16:25:19 +0700

    chore: separate API client for ease to mock test

---
+++ TCA Tutorial/Features/Counter/API/TodoClientAPI.swift
@@ -0,0 +1,30 @@
+//
+//  TodoClientAPI.swift
+//  TCA Tutorial
+//
+//  Created by ENB Mac Mini on 12/12/25.
+//
+
+import ComposableArchitecture
+import Foundation
+
+struct TodoClientAPI {
+    var fetch: (Int) async throws -> TodoDTO
+}
+
+extension TodoClientAPI: DependencyKey {
+    static let liveValue = Self(
+        fetch: { number in
+            let (data, _) = try await URLSession.shared
+                .data(from: URL(string: "https://jsonplaceholder.typicode.com/todos/\(number)")!)
+            return TodoDTO.decode(from: data)
+        }
+    )
+}
+
+extension DependencyValues {
+    var todoClientAPI: TodoClientAPI {
+        get { self[TodoClientAPI.self] }
+        set { self[TodoClientAPI.self] = newValue }
+    }
+}
