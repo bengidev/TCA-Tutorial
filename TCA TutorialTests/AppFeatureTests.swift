1cd82a4b13c4abcbe7adb2e02c33b5e7d39ce783
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2025-12-15 16:42:41 +0700

    chore: add unit test for behavior parent reducer

---
+++ TCA TutorialTests/AppFeatureTests.swift
@@ -0,0 +1,25 @@
+//
+//  AppFeatureTests.swift
+//  TCA Tutorial
+//
+//  Created by ENB Mac Mini on 15/12/25.
+//
+
+import ComposableArchitecture
+import Testing
+
+@testable import TCA_Tutorial
+
+@MainActor
+struct AppFeatureTests {
+    @Test
+    func incrementInFirstTab() async {
+        let store = TestStore(initialState: AppFeature.State()) {
+            AppFeature()
+        }
+
+        await store.send(\.tab1.incrementButtonTapped) {
+            $0.tab1.count = 1
+        }
+    }
+}
