603fc3924f0fa933821af8d780c5b8ab5dcadb84
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2025-12-15 16:42:03 +0700

    chore: joint multiple reducers into single parent

---
+++ TCA Tutorial/Features/Home/Domain/AppReducer.swift
@@ -0,0 +1,30 @@
+import ComposableArchitecture
+
+@Reducer
+struct AppReducer {
+    @ObservableState
+    struct State: Equatable {
+        var tab1 = CounterReducer.State()
+        var tab2 = CounterReducer.State()
+    }
+
+    enum Action {
+        case tab1(CounterReducer.Action)
+        case tab2(CounterReducer.Action)
+    }
+
+    var body: some ReducerOf<Self> {
+        Scope(state: \.tab1, action: \.tab1) {
+            CounterReducer()
+        }
+        
+        Scope(state: \.tab2, action: \.tab2) {
+            CounterReducer()
+        }
+        
+        Reduce { state, action in
+            // Core logic of the app feature
+            .none
+        }
+    }
+}
